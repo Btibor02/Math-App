eb6095e61dc188b7a604dd9ed4f3a52853fd571a
Tibor Blascsok <tiborblascsok02@gmail.com>
2025-10-15 16:27:45 +0200

    Add buttons and text

--- app/src/main/java/com/example/mathquest/MainActivity.kt
+++ app/src/main/java/com/example/mathquest/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -61,6 +62,25 @@ fun MainScreen() {
             painter = painterResource(id = R.drawable.main_screen_pic_without_background),
             contentDescription = "Logo",
             modifier = Modifier.size(200.dp)
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Button(onClick = {/* TODO: navigate to login */ }) {
+            Text("Login")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {/* TODO: navigate to register */ }) {
+            Text("Register")
+        }
+
+        Text(
+            text = "Have a teacher account? Sign in",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Medium,
+            color = Color(0xFF3F51B5)
         )
     }
 }
