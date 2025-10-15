7a2b6117bf8b67906d0139f5a94b9df0c6ad5648
Tibor Blascsok <tiborblascsok02@gmail.com>
2025-10-16 01:34:48 +0200

    Current login design

--- app/src/main/java/com/example/mathquest/screens/LoginScreen.kt
+++ app/src/main/java/com/example/mathquest/screens/LoginScreen.kt
@@ -1,2 +1,127 @@
 package com.example.mathquest.screens
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.mathquest.R
+import com.example.mathquest.navigation.Screen
+
+@Composable
+fun LoginScreen(navController: NavController) {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            "Login",
+            fontSize = 60.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily(Font(R.font.nunito_extrabold)),
+            color = Color(0xFF214A80)
+        )
+
+        Spacer(modifier = Modifier.height(60.dp))
+
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Username") },
+            modifier = Modifier
+                .height(80.dp)
+                .width(350.dp),
+            shape = RoundedCornerShape(15.dp),
+            textStyle = TextStyle(fontSize = 40.sp)
+        )
+
+        Spacer(Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            modifier = Modifier
+                .height(80.dp)
+                .width(350.dp),
+            shape = RoundedCornerShape(15.dp),
+            textStyle = TextStyle(fontSize = 40.sp),
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(Modifier.height(32.dp))
+
+        Button(
+            onClick = { navController.navigate(Screen.Main.route) },
+            modifier = Modifier
+                .height(80.dp)
+                .width(350.dp),
+            shape = RoundedCornerShape(15.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF4DA6FF)
+            )
+        ) {
+            Text(
+                "Log in",
+                fontFamily = FontFamily(Font(R.font.nunito_extrabold)),
+                fontSize = 40.sp
+            )
+        }
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        SignUpPrompt(onSignUpClick = {
+            /* TODO: navigate to register */
+        })
+    }
+}
+
+@Composable
+fun SignUpPrompt(onSignUpClick: () -> Unit) {
+    val annotatedString = buildAnnotatedString {
+        append("New here? ")
+        pushStringAnnotation(tag = "SIGN_UP", annotation = "sign_up")
+        withStyle(
+            style = SpanStyle(
+                color = Color(0xFF214A80),
+                textDecoration = TextDecoration.Underline,
+                fontWeight = FontWeight.Medium
+            )
+        ) {
+            append("Sign up")
+        }
+        pop()
+    }
+
+    Text(
+        text = annotatedString,
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Medium,
+        color = Color.Black,
+        modifier = Modifier.clickable {
+            /* TODO: navigate to login */
+            onSignUpClick()
+        }
+    )
+}
